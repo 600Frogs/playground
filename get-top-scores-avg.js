07a733c650f654c37e4dd72ab8dc094f91a3dfd9
Zak Avery <zavery1992@gmail.com>
2018-09-04 13:06:06 +1000

    Added comments for readability

--- get-top-scores-avg.js
+++ get-top-scores-avg.js
@@ -1,10 +1,14 @@
-
+//Parameter: an array of objects containing student ID's and test scores.  Example: [{id: 1, score: 86}]
+//Returns: array of unique student ID's
 function getUniqueStudents(scores) {
     return scores
         .map(e => e.id)
         .filter((element, index, array) => array.indexOf(element) === index)
 }
 
+//First parameter: an array of objects containing student ID's and test scores.  Example: [{id: 1, score: 86}]
+//Second Parameter: a number matching a student ID contained in the scores array.
+//Returns: an array containing the top five scores for that user ID
 function getTopFiveScores(scores, studentID) {
     return scores
         .filter(e => e.id == studentID)
@@ -24,6 +28,7 @@ function getTopFiveScores(scores, studentID) {
 
 }
 
+//Takes array of numbers and returns the average (num)
 function getAverage(arr) {
     return arr.reduce((t, c) => t + c) / arr.length
 }
@@ -44,6 +49,6 @@ const scores = [
     { id: 1, score: 89 },
     { id: 1, score: 99 }];
 
-
+//Print everything out
 getUniqueStudents(scores)
     .forEach(id => console.log("ID: " + id + " -> AvgScore: " + getAverage(getTopFiveScores(scores, id))))
